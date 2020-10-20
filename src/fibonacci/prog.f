7c5e5fc1e660900816d37c3b02e98cd55a4ad672
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-19 19:20:54 -0700

    fibonacci: rename variables, comment, and fill in sequence column

--- src/fibonacci/prog.f
+++ src/fibonacci/prog.f
@@ -1,20 +1,30 @@
-C     FIBONACCI.F                                                               
-C     COPYRIGHT (C) 2020 HUGH COLEMAN                                           
-C                                                                               
-C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO INVESTIGATE THE   
-C     FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE EARLY DAYS OF COMPUTING. 
-C     IT IS RELEASED UNDER THE MIT LICENSE (SEE LICENSE.)                       
-                                                                                
- 10   FORMAT ( 1I6 )                                                            
- 20   READ 10, N                                                                
-      IF (N) 90,90,30                                                           
- 30   IFIBA = 0                                                                 
-      IFIBB = 1                                                                 
-      DO 40 I=1,N                                                               
-      ITMP = IFIBB                                                              
-      IFIBB = IFIBB + IFIBA                                                     
-      IFIBA = ITMP                                                              
- 40   CONTINUE                                                                  
-      PRINT 10, IFIBB                                                           
-      GOTO 20                                                                   
- 90   STOP                                                                      
+C     FIBONACCI.F                                                       1       
+C     COPYRIGHT (C) 2020 HUGH COLEMAN                                   2       
+C                                                                       3       
+C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO           4       
+C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  5       
+C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE6       
+C     LICENSE.)                                                         7       
+                                                                        8       
+ 10   FORMAT ( 1I6 )                                                    9       
+                                                                        10      
+C     READ A 6 DIGIT INTEGER FROM THE CARD READER. EXIT THE PROGRAM IF  11      
+C     THE QUERIED INTEGER IS 0.                                         12      
+ 20   READ 10, N                                                        13      
+      IF (N) 9999, 9999, 30                                             14      
+                                                                        15      
+C     COMPUTE THE FIBONACCI SEQUENCE BY MAINTAINING TWO INTEGER         16      
+C     VARIABLES IPREV (PREVIOUS) AND IACCM (ACCUMULATOR). EACH ITERATION17    
+C     OF THE LOOP, SET IPREV TO IACCM AND IACCM TO IACCM + IPREV        18      
+ 30   IPREV = 0                                                         19      
+      IACCM = 1                                                         20      
+      DO 40 I=1, N                                                      21      
+      ITMP = IACCM                                                      22      
+      IACCM = IACCM + IPREV                                             23      
+      IPREV = ITMP                                                      24      
+ 40   CONTINUE                                                          25      
+                                                                        26      
+      PRINT 10, IACCM                                                   27      
+      GOTO 20                                                           28      
+                                                                        29      
+ 9999 STOP                                                              30      
