58697aa48c2bfe2ea74f9f3811f631a1e37b9543
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-19 19:20:29 -0700

    hello: fill in sequence column

--- src/hello/prog.f
+++ src/hello/prog.f
@@ -1,10 +1,11 @@
-C     HELLO.F                                                                   
-C     COPYRIGHT (C) 2020 HUGH COLEMAN                                           
-C                                                                               
-C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO INVESTIGATE THE   
-C     FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE EARLY DAYS OF COMPUTING. 
-C     IT IS RELEASED UNDER THE MIT LICENSE (SEE LICENSE.)                       
-                                                                                
- 10   FORMAT ( 13H HELLO, WORLD )                                              
-      PRINT 10                                                                  
-      STOP                                                                      
+C     HELLO.F                                                           1       
+C     COPYRIGHT (C) 2020 HUGH COLEMAN                                   2       
+C                                                                       3       
+C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO           4       
+C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  5       
+C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE6       
+C     LICENSE.)                                                         7       
+                                                                        8       
+ 10   FORMAT ( 13H HELLO, WORLD )                                       9      
+      PRINT 10                                                          10      
+      STOP                                                              11      
