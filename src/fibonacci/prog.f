127444cca8e05b11956e9f6f705b5d12ab311a9a
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-21 18:02:47 -0700

    fibonacci: modify variables to be floating-point

--- src/fibonacci/prog.f
+++ src/fibonacci/prog.f
@@ -6,33 +6,34 @@ C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  5
 C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE6       
 C     LICENSE.)                                                         7       
                                                                         8       
-      DIMENSION IFIB(23)                                                9       
-                                                                        10      
- 10   FORMAT ( 1I2 )                                                    11      
- 11   FORMAT ( 1I3, 1I6 )                                               12      
- 12   FORMAT ( 1I3, 11H TOO LARGE. )                                    13      
- 13   FORMAT ( 9H  N  F(N) )                                            14      
+ 10   FORMAT ( 1I2 )                                                    9       
+ 11   FORMAT ( 14H  N       F(N) )                                      10      
+ 12   FORMAT ( 1I3, 1F11.0 )                                            11      
+ 13   FORMAT ( 1I3, 11H TOO LARGE. )                                    12      
+                                                                        13      
+      PRINT 11                                                          14      
                                                                         15      
-C     PRECOMPUTE FIRST 23 FIBONACCI NUMBERS, AS THEY ARE THE ONLY ONES  16      
-C     THAT ARE SMALL ENOUGH TO FIT IN AN INTEGER TYPE VARIABLE.         17      
-      IFIB(1) = 0                                                       18      
-      IFIB(2) = 1                                                       19      
-      DO 20 I = 3, 23                                                   20      
-          IFIB(I) = IFIB(I-1) + IFIB(I-2)                               21      
- 20   CONTINUE                                                          22      
+C     READ A 2 DIGIT INTEGER FROM THE CARD READER. EXIT THE PROGRAM IF  16      
+C     THE QUERIED INTEGER IS -1. REJECT QUERIES THAT WILL LEAD TO       17      
+C     FLOATING-POINT OVERFLOWS.                                         18      
+ 20   READ 10, N                                                        19      
+      NC = N                                                            20      
+      IF (N+1) 9999, 9999, 21                                           21      
+ 21   IF (N-40) 30, 30, 130                                             22      
                                                                         23      
-      PRINT 13                                                          24      
-                                                                        25      
-C     READ A 2 DIGIT INTEGER FROM THE CARD READER. EXIT THE PROGRAM IF  26      
-C     THE QUERIED INTEGER IS 0. REJECT QUERIES THAT ARE TOO LARGE.      27      
- 30   READ 10, N                                                        28      
-      IF (N) 9999, 9999, 31                                             29      
- 31   IF (N-23) 40, 40, 50                                              30      
-                                                                        31      
- 40   PRINT 11, N, IFIB(N)                                              32      
-      GOTO 30                                                           33      
-                                                                        34      
- 50   PRINT 12, N                                                       35      
-      GOTO 30                                                           36      
-                                                                        37      
- 9999 STOP                                                              38      
+ 30   FACC = 0                                                          24      
+      FNXT = 1                                                          25      
+ 31   IF (N) 9999, 120, 32                                              26      
+ 32   FTMP = FNXT                                                       27      
+      FNXT = FACC + FNXT                                                28      
+      FACC = FTMP                                                       29      
+      N = N - 1                                                         30      
+      GO TO 31                                                          31      
+                                                                        32      
+ 120  PRINT 12, NC, FACC                                                33      
+      GOTO 20                                                           34      
+                                                                        35      
+ 130  PRINT 13, NC                                                      36      
+      GOTO 20                                                           37      
+                                                                        38      
+ 9999 STOP                                                              39      
