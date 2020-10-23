441dd94887710ebd68070c962957c20711ebdfe6
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2020-10-23 14:33:50 -0700

    fibonacci: precompute sequence and store in subscripted vector

--- src/fibonacci/prog.f
+++ src/fibonacci/prog.f
@@ -6,34 +6,35 @@ C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  5
 C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE6       
 C     LICENSE.)                                                         7       
                                                                         8       
- 10   FORMAT ( 1I2 )                                                    9       
- 11   FORMAT ( 14H  N       F(N) )                                      10      
- 12   FORMAT ( 1I3, 1F11.0 )                                            11      
- 13   FORMAT ( 1I3, 11H TOO LARGE. )                                    12      
-                                                                        13      
-      PRINT 11                                                          14      
+      DIMENSION FIB(41)                                                 9       
+                                                                        10      
+ 10   FORMAT ( 1I2 )                                                    11      
+ 11   FORMAT ( 14H  N       F(N) )                                      12      
+ 12   FORMAT ( 1I3, 1F11.0 )                                            13      
+ 13   FORMAT ( 1I3, 11H TOO LARGE. )                                    14      
                                                                         15      
-C     READ A 2 DIGIT INTEGER FROM THE CARD READER. EXIT THE PROGRAM IF  16      
-C     THE QUERIED INTEGER IS -1. REJECT QUERIES THAT WILL LEAD TO       17      
-C     FLOATING-POINT OVERFLOWS.                                         18      
- 20   READ 10, N                                                        19      
-      NC = N                                                            20      
-      IF (N+1) 9999, 9999, 21                                           21      
- 21   IF (N-40) 30, 30, 130                                             22      
-                                                                        23      
- 30   FACC = 0                                                          24      
-      FNXT = 1                                                          25      
- 31   IF (N) 9999, 120, 32                                              26      
- 32   FTMP = FNXT                                                       27      
-      FNXT = FACC + FNXT                                                28      
-      FACC = FTMP                                                       29      
-      N = N - 1                                                         30      
-      GO TO 31                                                          31      
+      PRINT 11                                                          16      
+                                                                        17      
+C     PRE-COMPUTE FIBONACCI SEQUENCE AND STORE IN THE FIB() VECTOR.     18      
+      FIB(1) = 0                                                        19      
+      FIB(2) = 1                                                        20      
+                                                                        21      
+      DO 20 N=3, 41                                                     22      
+          FIB(N) = FIB(N - 1) + FIB(N - 2)                              23      
+ 20   CONTINUE                                                          24      
+                                                                        25      
+C     READ A 2 DIGIT INTEGER FROM THE CARD READER. EXIT THE PROGRAM IF  26      
+C     THE QUERIED INTEGER IS -1. REJECT QUERIES THAT ARE TOO LARGE.     27      
+ 30   READ 10, N                                                        28      
+      NC = N                                                            29      
+      IF (N+1) 9999, 9999, 31                                           30      
+ 31   IF (N-40) 120, 120, 130                                           31      
                                                                         32      
- 120  PRINT 12, NC, FACC                                                33      
-      GOTO 20                                                           34      
+ 120  PRINT 12, NC, FIB(N+1)                                            33      
+      GOTO 30                                                           34      
                                                                         35      
- 130  PRINT 13, NC                                                      36      
-      GOTO 20                                                           37      
-                                                                        38      
- 9999 STOP                                                              39      
+C     "TOO LARGE."                                                      36      
+ 130  PRINT 13, NC                                                      37      
+      GOTO 30                                                           38      
+                                                                        39      
+ 9999 STOP                                                              40      
