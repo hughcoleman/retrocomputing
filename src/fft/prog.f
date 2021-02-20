f40f89074b6f9189543a71405f8cce37230cbdb7
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2021-02-20 13:37:51 -0800

    inserted header

--- src/fft/prog.f
+++ src/fft/prog.f
@@ -1,3 +1,11 @@
+C     FFT.F                                                        
+C     COPYRIGHT (C) 2021 HUGH COLEMAN                                    
+C                                                                       
+C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO           
+C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  
+C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE
+C     LICENSE.)                                                         
+
 C0000*------------------------------------------------------------------00000000
 C     THE FAST FOURIER TRANSFORM
 C0000*------------------------------------------------------------------00000000
