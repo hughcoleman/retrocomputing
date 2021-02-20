a9aeca78067415773fc827803071facf9f74ac40
hughcoleman <33557709+hughcoleman@users.noreply.github.com>
2021-02-20 13:44:04 -0800

    inserted header

---
+++ src/torus/prog.f
@@ -0,0 +1,8 @@
+C     TORUS.F                                                    
+C     COPYRIGHT (C) 2020 HUGH COLEMAN                                   
+C                                                                       
+C     THIS FILE IS PART OF HUGHCOLEMAN/FORTRAN, AN ATTEMPT TO           
+C     INVESTIGATE THE FORTRAN PROGRAMMING LANGUAGE AND LEARN ABOUT THE  
+C     EARLY DAYS OF COMPUTING. IT IS RELEASED UNDER THE MIT LICENSE (SEE
+C     LICENSE.)                                                        
+ 
